9d94f05fa8c1872f7cae6c63a7324c99618dab3e
zhm022726 <zanghuiming@gmail.com>
2015-03-18 16:17:17 +0800

    study git

---
+++ com/git/testing/a.java
@@ -0,0 +1,30 @@
+Use this document to assist you in performing the day-to-day tasks of client,
+contact, activity, and opportunity management in SalesConnect.
+
+Sellers
+
+SalesConnect will play a large role in IBM Seller'S workday. It enables you to perform opportunity management tasks, as well as:
+
+Collaborate (real-time) with team members through microblogs, activity streams, Lotus Notes integration, and Connections.
+Perform many tasks on your mobile device
+Perform detailed searches and run reports
+Perform many tasks directly in SalesConnect based on the linkages with key productivity tools in IBM (360 degree view of the client, ISP, Win Plan, etc.)
+Locate recommended experts and documents
+
+Sales teams around the world already use SalesConnect to manage their opportunities. The application saves sellers time in progressing deals. It streamlines opportunity management tasks, leverages collaboration opportunities through integration with Client ConnectionHUBs and Connections communities, and offers mobile and social capabilities that go way beyond opportunity management.
+
+
+
+Use Javascript MVC in front end , PHP in server end,to develop a CRM application SalesConnect for IBM sales(base on sugarcrm).
+Sales teams around the world use SalesConnect to manage their opportunities. The application saves sellers time in progressing deals. It streamlines opportunity management tasks, leverages collaboration opportunities through integration with Client ConnectionHUBs and Connections communities
+
+使用javascriptMCV
+SalesConnect是基于SugarCRM开发的CRM应用系统.前段使用JavascriptMVC作为前段控制器,服务端使用PHP处理来自Web/mobile的请求.
+IBM Seles使用这套系统帮助工作提高效率,管理跟踪客户商机,客户等等.赢得订单.
+
+
+
+SalesConnect play a large role in IBM Seller'S workday. like opportunity management,performing the day-to-day tasks of client,
+contact, activity
+
+国际商业机器中国有限公司
